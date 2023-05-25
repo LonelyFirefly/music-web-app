d221c1afc55893d65ecae6c9e45839b77b89f1ab
LonelyFirefly <Itisperfectworld@gmail.com>
2023-05-25 13:38:03 +0400

    add the table, th and tr to the playlist items

--- src/Components/SongRadioPlaylistItem/SongRadioPlaylistItem.js
+++ src/Components/SongRadioPlaylistItem/SongRadioPlaylistItem.js
@@ -1,33 +1,54 @@
 import { convertFromMsToSec } from "@/utils/convertFromMsToSec";
 import { getSmallestImageFromAlbum } from "@/utils/getSmallestImageFromAlbum";
 
-export function SongRadioPlaylistItem({ song, key }) {
+export function SongRadioPlaylistItem({ song, key, index }) {
 	const albumImages = song.album.images;
 	const albumCoverUrl = getSmallestImageFromAlbum(albumImages).url;
 	const songName = song.name;
 	const artistName = song.artists[0].name;
 	const albumName = song.album.name;
-	const songDuration = song.duration_ms;
+	const songDuration = convertFromMsToSec(song.duration_ms);
+	// return (
+	// 	<li>
+	// 		<div className="grid grid-cols-6 pt-2.5 px-5 pb-1.5 align-middle text-xs font-normal text-gray-400">
+	// 			<div className="col-span-3 flex gap-3.5">
+	// 				<img
+	// 					alt="album cover"
+	// 					src={albumCoverUrl}
+	// 					className="w-9 h-fit self-center"
+	// 				/>
+	// 				<div>
+	// 					<p className="text-sm font-semibold text-white">
+	// 						{songName}
+	// 					</p>
+	// 					<p>{artistName}</p>
+	// 				</div>
+	// 			</div>
+	// 			<p> {albumName} </p>
+	// 			<p>date added</p>
+	// 			<p> {convertFromMsToSec(songDuration)} </p>
+	// 		</div>
+	// 	</li>
+	// );
 	return (
-		<li>
-			<div className="grid grid-cols-6 pt-2.5 px-5 pb-1.5 align-middle text-xs font-normal text-gray-400">
-				<div className="col-span-3 flex gap-3.5">
-					<img
-						alt="album cover"
-						src={albumCoverUrl}
-						className="w-9 h-fit self-center"
-					/>
-					<div>
-						<p className="text-sm font-semibold text-white">
-							{songName}
-						</p>
-						<p>{artistName}</p>
-					</div>
+		<tr key={song.id}>
+			<td>{index + 1}</td>
+			<td className="flex gap-4">
+				<img
+					alt="album cover"
+					src={albumCoverUrl}
+					className="w-9 h-fit self-center"
+				/>
+				<div>
+					<p className="text-sm font-semibold text-white">
+						{songName}
+					</p>
+					<p>{artistName}</p>
 				</div>
-				<p> {albumName} </p>
-				<p>date added</p>
-				<p> {convertFromMsToSec(songDuration)} </p>
-			</div>
-		</li>
+			</td>
+			<td>{albumName}</td>
+			<td> date added </td>
+			<td>{songDuration}</td>
+		</tr>
 	);
 }
--- src/Components/SongRadioPlaylistItems/SongRadioPlaylistItems.js
+++ src/Components/SongRadioPlaylistItems/SongRadioPlaylistItems.js
@@ -1,13 +1,40 @@
 import { SongRadioPlaylistItem } from "../SongRadioPlaylistItem/SongRadioPlaylistItem";
 
 export function SongRadioPlaylistItems({ playedSongsList }) {
+	// return (
+	// 	<div className="bg-song-dark">
+	// 		<ol>
+	// 			{playedSongsList.map((song) => (
+	// 				<SongRadioPlaylistItem song={song} key={song.id} />
+	// 			))}
+	// 		</ol>
+	// 	</div>
+	// );
 	return (
-		<div className="bg-song-dark">
-			<ol>
-				{playedSongsList.map((song) => (
-					<SongRadioPlaylistItem song={song} key={song.id} />
+		<table className="bg-song-dark ml-5 w-full">
+			<thead>
+				<tr className="text-left">
+					<th>#</th>
+					<th>Title</th>
+					<th>Album</th>
+					<th>Date Added</th>
+					<th>
+						<img
+							alt="duration icon"
+							src="/images/dark-theme/icon-dark-theme-duration.svg"
+						/>
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+				{playedSongsList.map((song, index) => (
+					<SongRadioPlaylistItem
+						song={song}
+						key={song.id}
+						index={index}
+					/>
 				))}
-			</ol>
-		</div>
+			</tbody>
+		</table>
 	);
 }
