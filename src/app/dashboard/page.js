ebfe0b1768379b2029015d1826b7d715af45a60c
LonelyFirefly <Itisperfectworld@gmail.com>
2023-05-27 15:31:28 +0400

    fix

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,4 +1,4 @@
-import { RecentlyPlayedSongs } from "@/src/Components/RecentlyPlayedSongs/RecentlyPlayedSongs";
+import { RecentlyPlayedSongs } from "@/src/Components/RecentlyPlayedSongs/RecentlyPlayedSongs.js";
 import { playedSongsList } from "@/FakeData/playedSongsList";
 
 export default function Page() {
