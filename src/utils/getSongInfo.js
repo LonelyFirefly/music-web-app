95fef5d3ad073072497f8bf27b12ebb9a9746b9b
LonelyFirefly <Itisperfectworld@gmail.com>
2023-06-02 12:39:42 +0400

    ...

--- src/utils/getSongInfo.js
+++ src/utils/getSongInfo.js
@@ -5,6 +5,7 @@ export function getSongInfo() {
 	// returns 1 object(song) from an array of Obejct(songs) based on the song's ID
 	const { radioSongs, currentSongId } = useContext(SongContext);
 
+	//define variables so that the return statement doesn't give an error
 	let songName,
 		songAlbumCoverImg,
 		songArtistName,
