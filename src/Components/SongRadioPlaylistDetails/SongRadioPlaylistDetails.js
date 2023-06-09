662a33ba2b9d6a50ea30e6376310597b6222333d
LonelyFirefly <Itisperfectworld@gmail.com>
2023-06-09 12:00:29 +0400

    manage to get acces token, but it is expired

--- src/Components/SongRadioPlaylistDetails/SongRadioPlaylistDetails.js
+++ src/Components/SongRadioPlaylistDetails/SongRadioPlaylistDetails.js
@@ -4,6 +4,64 @@ import { useContext, useState } from "react";
 import { LikeSong } from "../LikeSong/LikeSong";
 import { LikeAlbum } from "../LikeAlbum/LikeAlbum";
 
+// https://open.spotify.com/artist/0qT79UgT5tY4yudH9VfsdT?si=BbVUo51qSIuMayWBmPBGBg
+
+async function getAccessToken() {
+	const res = await fetch("https://accounts.spotify.com/api/token", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+		body: new URLSearchParams({
+			grant_type: "client_credentials",
+			client_id: "8da9ef4ee3d3438b9295a953325597af",
+			client_secret: "108820275ac9482e98f484b5dcd461dc",
+		}),
+	});
+
+	if (!res.ok) {
+		throw new Error("Failed to fetch data");
+	}
+
+	const data = await res.json();
+	console.log(`access token is ${data.expires_in}`);
+	return data.access_token;
+}
+
+async function getArtist() {
+	const accessToken = await getAccessToken();
+	const res = await fetch(
+		"https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg",
+		{
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${accessToken}`,
+			},
+		}
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			try {
+				if (data.error) {
+					console.log("ERROR !!!");
+					console.log(data.error.message);
+				}
+				// Handle the response data here
+				console.log("Not Erorr");
+				console.log(data);
+			} catch (error) {
+				console.log("Error in data");
+				console.log(error);
+			}
+		})
+		.catch((error) => {
+			// Handle any errors here
+			console.log("Error");
+			console.error(error);
+		});
+	return res;
+}
+
 export function SongRadioPlaylistDetails() {
 	const { isPlaying, setIsPlaying, songName } = useContext(SongContext);
 	const pathNameSongRadio = usePathname();
@@ -11,6 +69,9 @@ export function SongRadioPlaylistDetails() {
 	console.log(
 		`Pathname: ${pathNameSongRadio}; searchParamsImagePath: ${searchParamsImagePath}`
 	);
+	const artist = getArtist();
+	console.log("Artist:");
+	console.log(artist);
 
 	function handlePlayClick() {
 		setIsPlaying(!isPlaying);
