2e21e1f18aa1ab7c362f587704b9e04d730a6fc3
LonelyFirefly <Itisperfectworld@gmail.com>
2023-06-14 13:22:59 +0400

    add MUI

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -12,6 +13,21 @@ export default function RootLayout({ children }) {
 	return (
 		// as a root layout, it should contain html and body tags
 		<html lang="en">
+			<Head>
+				<title>Spotiy Music Web App</title>
+				<meta
+					name="viewport"
+					content="initial-scale=1, width=device-width"
+				/>
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+				/>
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/icon?family=Material+Icons"
+				/>
+			</Head>
 			<body className={inter.className}>{children}</body>
 		</html>
 	);
